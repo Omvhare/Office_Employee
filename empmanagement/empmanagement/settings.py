5600bc523daa618d0f6a50e83b5b529348bf1a9c
om vhare <om143vhare00@gmail.com>
2025-07-16 01:54:35 +0530

    Updated files after initial push

--- empmanagement/empmanagement/settings.py
+++ empmanagement/empmanagement/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'empmanagement.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'empmanagement' / 'templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
